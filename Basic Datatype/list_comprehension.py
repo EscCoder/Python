b999782919fdb75c7a9903700f20e5eb626141db
Pallavi <101661582+EscCoder@users.noreply.github.com>
2022-10-07 20:39:26 +0530

    v1.1

---
+++ Basic Datatype/list_comprehension.py
@@ -0,0 +1,13 @@
+"""
+You are given three integers x ,y and z representing the dimensions of a cuboid along with an integer n.
+Print a list of all possible coordinates given by (i,j,k)  on a 3D grid where the sum of i+j+k is not equal to n
+Use list comprehensions rather than multiple loops, as a learning exercise.
+
+
+Solution
+
+take x,y,z as input
+by permutation combination make possible coordinate combinations
+check if sum of coordinates is equal to n or not and if not add them in a list
+
+"""
