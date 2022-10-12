3bf2201db3f793df66f36e03272a5d988899eedb
Pallavi <101661582+EscCoder@users.noreply.github.com>
2022-10-12 16:38:08 +0530

    v1.0

---
+++ Fuctions/km_to_m.py
@@ -0,0 +1,24 @@
+"""
+to convert kilometer into meter
+"""
+----------------
+def meter(km):
+  m = km * 1000
+  return m
+
+km = int(input("Enter value in km :"))
+print(km,"km converted into meters is ",meter(km),"m")
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+"""
+to convert meter into kilometer
+"""
+----------------
+def kilometer(m):
+  km = m * 1000
+  return km
+
+m = int(input("Enter value in km :"))
+print(m,"m converted into kilometers is ",kilometer(m),"km")
+
+---------------------------------------------------------------------------------------------------------------------------------------------------------
